6fc8a7a7e12dccc56b35c2e70c68fefe2810b670
null0124 <cat42015g@gmail.com>
2020-05-13 01:59:34 +0900

    辞書順最小にする部分を削除

--- kyopro/Library/Graph/dijkstra_path.cpp
+++ kyopro/Library/Graph/dijkstra_path.cpp
@@ -1,23 +1,22 @@
-
-
-
 template<typename T>
-vector<T> dijkstra(const vector<vector<pair<int, T>>>& graph, vector<int>& path, const int& v, const int& g, const int& n, const T inf) {
+vector<T> dijkstra(const vector<vector<pair<int, T>>>& graph, vector<int>& path, const int& v, const int& g, const int& n, const T Inf) {
 	priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> priq;
 	vector<T> res(n);
 	vector<int> prev(n);
 	fill(all(prev), -1);
-	fill(all(res), inf);
+	fill(all(res), Inf);
 	priq.push({ 0, v });
 	res[v] = 0;
 	int top;
 	while (!priq.empty()) {
-		top = priq.top().second;
+		auto now = priq.top();
+		top = now.second;
 		priq.pop();
+		if (res[top] < now.first)continue;
 		for (const auto& aa : graph[top]) {
 			if (res[top] + aa.second > res[aa.first])continue;
 			else if (res[top] + aa.second == res[aa.first]) {
-				prev[aa.first] = min(top, prev[aa.first]);
+				//prev[aa.first] = min(top, prev[aa.first]);
 				continue;
 			}
 			res[aa.first] = aa.second + res[top];
@@ -27,6 +26,8 @@ vector<T> dijkstra(const vector<vector<pair<int, T>>>& graph, vector<int>& path,
 	}
 
 	for (int i = g; i != -1; i = prev[i])path.push_back(i);
+
+	reverse(all(path));
 
 	return res;
 }
