cfb646d8639a776e1a9acd7bf45c424ebbe6bd3a
null0124 <cat42015g@gmail.com>
2020-05-12 19:37:15 +0900

    add template test (A + B)

---
+++ kyopro/test/template_yosupo-judge.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../Library/template/template.cpp"
+
+int main() {
+
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d\n", a + b);
+
+	Please AC;
+}
