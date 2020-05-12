df8c23480699a16b6faea01fc325e5656eb2a78a
null0124 <cat42015g@gmail.com>
2020-05-12 19:47:52 +0900

    hey

---
+++ kyopro/test/unionfind_yosupo-judge.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "../Library/template/template.cpp"
+
+#include "../Library/DataStructure/unionfind.cpp"
+
+int main() {
+	int n, q;
+	scanf("%d%d", &n, &q);
+	unionfind uni(n);
+	while (q--) {
+		int t, u, v;
+		scanf("%d%d%d", &t, &u, &v);
+		if (t)printf("%d\n", uni.same(u, v));
+		else uni.unite(u, v);
+	}
+}
