8104392fa4f8ccbc8dc6aa3a2d8df00e10f2ed8e
null0124 <cat42015g@gmail.com>
2020-05-16 20:30:00 +0900

    fix doubling

--- kyopro/library/others/doubling.cpp
+++ kyopro/library/others/doubling.cpp
@@ -2,8 +2,9 @@
 
 	int n, log_k;
 	vector<vector<int>> a;
+	const function<int(int, int)> f1, f2;
 
-	doubling(int n, ll k) : n(n) {
+	doubling(int n, ll k, function<int(int, int)> f1, function<int(int, int)> f2) : n(n), f1(f1), f2(f2) {
 		log_k = ceil(log2(k));
 		a.assign(log_k + 1, vector<int>(n, -1));
 	}
@@ -17,7 +18,7 @@
 		for (int i = 0; i < log_k; ++i) {
 			for (int j = 0; j < n; ++j) {
 				if (a[i][j] == -1) a[i + 1][j] = -1;
-				else a[i + 1][j] = a[i][a[i][j]];
+				else a[i + 1][j] = f1(a[i + 1][j], a[i][a[i][j]]);
 			}
 		}
 	}
@@ -26,7 +27,7 @@
 	int query(int k, ll x) {
 		for (int i = log_k; i >= 0; --i) {
 			if (k == -1)break;
-			else if ((x >> (ll)i) & 1LL) k = a[i][k];
+			else if ((x >> (ll)i) & 1LL) k = f2(k, a[i][k]);
 		}
 		return k;
 	}
