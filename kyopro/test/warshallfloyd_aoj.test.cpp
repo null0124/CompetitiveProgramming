6e9d6ffac8d7973a628cc7b8d632429b6cd9119c
null0124 <cat42015g@gmail.com>
2020-05-17 18:12:36 +0900

    add test of warshallfloyd

---
+++ kyopro/test/warshallfloyd_aoj.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_C"
+
+#include "../library/template/template.cpp"
+#include "../library/graph/warshallfloyd.cpp"
+
+int main() {
+
+	int v, e;
+	scanf("%d%d", &v, &e);
+	vector<vector<ll>> g(v, vector<ll>(v));
+	rep(i, v)fill(all(g[i]), LINF / 2);
+	rep(i, v)g[i][i] = 0;
+	while (e--) {
+		int s, t, d;
+		scanf("%d%d%d", &s, &t, &d);
+		g[s][t] = d;
+	}
+	WarshallFloyd(v, g);
+	rep(i, v) {
+		if (g[i][i] < 0) {
+			puts("NEGATIVE CYCLE");
+			Please AC;
+		}
+	}
+	rep(i, v) {
+		rep(j, v) {
+			if (g[i][j] >= LINF / 4)printf("INF");
+			else printf("%lld", g[i][j]);
+			if (j != v - 1)printf(" ");
+			else puts("");
+		}
+	}
+
+	Please AC;
+}
