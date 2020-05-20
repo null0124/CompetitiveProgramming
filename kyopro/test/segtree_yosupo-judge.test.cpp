0c7baf8f5297c0fc00201b1f124b7ca801744a2b
null0124 <cat42015g@gmail.com>
2020-05-21 02:14:52 +0900

    ウッウ

--- kyopro/test/segtree_yosupo-judge.test.cpp
+++ kyopro/test/segtree_yosupo-judge.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+﻿#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
 
 #include "../library/template/template.cpp"
 
