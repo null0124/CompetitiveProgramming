eb4f49097505e4b021a4c795970cb179d1b83513
null0124 <cat42015g@gmail.com>
2020-08-03 02:19:54 +0900

    add topological sort

---
+++ kyopro/library/graph/topological_sort.cpp
@@ -0,0 +1,24 @@
+﻿/*
+* @title topological-sort
+* @docs kyopro/docs/topological_sort.md
+*/
+
+template<typename T = int>
+vector<int> topological_sort(graph<T>& g, const int& v) {
+	vector<int> ret, in(v);
+
+	queue<int> que;
+	rep(i, v) for (const auto& aa : g[i])++in[aa.to];
+	rep(i, v)if (not in[i])que.push(i);
+
+	while (not que.empty()) {
+		int top = que.front();
+		que.pop();
+		for (const auto& aa : g[top]) {
+			--in[aa.to];
+			if (not in[aa.to])que.push(aa.to);
+		}
+		ret.emplace_back(top);
+	}
+	return ret;
+}
