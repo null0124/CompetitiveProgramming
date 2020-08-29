17970fbf2fd28fde3d593bfe6b1c82a367d73d99
null0124 <cat42015g@gmail.com>
2020-08-30 05:54:24 +0900

    OK

--- kyopro/library/math/fast-prime_factor.cpp
+++ kyopro/library/math/fast-prime_factor.cpp
@@ -1,1 +1,5 @@
-﻿//https://drken1215.hatenablog.com/entry/2018/09/24/194500
+﻿/*
+* @title 高速素因数分解
+* @docs kyopro/docs/fast-prime_factor.md
+*/
+//https://drken1215.hatenablog.com/entry/2018/09/24/194500
