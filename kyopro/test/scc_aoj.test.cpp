4521413cd5cfe350e8d018516341e0ab5c2001c6
null0124 <cat42015g@gmail.com>
2020-09-06 19:30:36 +0900

    a

---
+++ kyopro/test/scc_aoj.test.cpp
@@ -0,0 +1,26 @@
+﻿#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_3_C"
+
+
+#include "../library/template/template.cpp"
+
+#include "../library/graph/graph_template.cpp"
+#include "../library/graph/scc.cpp"
+
+
+int main() {
+
+	int v, e;
+	scanf("%d%d", &v, &e);
+	graph<int> g(v, true, false);
+	g.read(e, false);
+	stronglyconnectedcomponents<int> s(v, g);
+	s.build();
+	int q;
+	scanf("%d", &q);
+	while (q--) {
+		scanf("%d%d", &v, &e);
+		printf("%d\n", (s[v] == s[e]));
+	}
+
+	Please AC;
+}
