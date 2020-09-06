219384975876d42c3414732e7affc1163688cf19
null0124 <cat42015g@gmail.com>
2020-09-07 03:43:07 +0900

    add tree diameter

---
+++ kyopro/test/tree-diameter_aoj.test.cpp
@@ -0,0 +1,19 @@
+﻿#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_A"
+
+#include "../library/template/template.cpp"
+
+#include "../library/graph/graph_template.cpp"
+#include "../library/graph/tree-diameter.cpp"
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+	graph<int> g(n, false, true);
+	g.read(n - 1, false);
+	int ans = 0;
+	diameter(g, ans);
+	printf("%d\n", ans);
+
+	Please AC;
+}
---
+++ kyopro/test/tree-diameter_yosupo-judge.test.cpp
@@ -0,0 +1,20 @@
+﻿#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+
+#include "../library/template/template.cpp"
+
+#include "../library/graph/graph_template.cpp"
+#include "../library/graph/tree-diameter.cpp"
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+	graph<ll> g(n, false, true);
+	g.read(n - 1, false, "%lld");
+	ll ans = 0;
+	auto v = diameter<ll>(g, ans);
+	printf("%lld %d\n", ans, (int)v.size());
+	for (const auto& aa : v)printf("%d ", aa);
+
+	Please AC;
+}
